324664edf16559c42ddba57dfd9c4e9a1b73195c
DhanaNugraha <dhananugraha1511@gmail.com>
2024-11-15 22:44:30 +0700

    feat: night mode

---
+++ index.js
@@ -0,0 +1,30 @@
+function nightMode() {
+  if (localStorage.getItem("theme") === "light") {
+    document.getElementById("body").style.backgroundColor = "#201e38";
+
+    document.getElementById("header").style.backgroundColor = "#594e37";
+
+    // localStorage.removeItem('theme');
+
+    localStorage.setItem("theme", "dark");
+  } else if (localStorage.getItem("theme") === "dark") {
+    document.getElementById("body").style.backgroundColor = "white";
+
+    document.getElementById("header").style.backgroundColor = "#ffc341";
+
+    localStorage.setItem("theme", "light");
+
+    // localStorage.removeItem('theme');
+    console.log("test");
+  } else {
+    document.getElementById("body").style.backgroundColor = "#201e38";
+
+    document.getElementById("header").style.backgroundColor = "#594e37";
+
+    localStorage.setItem("theme", "dark");
+
+    console.log("test else");
+
+    // localStorage.removeItem('theme');
+  }
+}
